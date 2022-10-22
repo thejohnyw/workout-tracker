8b09017e99a797e4ecdc9554b07e05dd9c2912e8
thejohnyw <john.y.wang.26@dartmouth.edu>
2022-10-22 00:38:15 -0400

    another cors origin link

--- backend/app.py
+++ backend/app.py
@@ -8,12 +8,14 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_cors import CORS
-#link: https://workout-watcher.herokuapp.com/
+
+
+#link to access backend: https://workout-watcher.herokuapp.com/
 
 app = Flask(__name__) # Configuring web app and templates with Flask
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kaswwsnrhvmuky:6bdc9cf2e1258c986450177327d385210fc5045c14d7dd1d7e059037ff8f38fc@ec2-35-170-146-54.compute-1.amazonaws.com:5432/d75bbdnufgb4ru' #Configuring PostgreSQL database with web app
 db = SQLAlchemy(app) #initalizing database
-cors = CORS(app, origins=["http://localhost:3000", "https://workout-watcher.me", "https://workout-watcher.netlify.app"]) #allows react (localhost:3000/deployed sites) to access flask app (localhost:5000)
+cors = CORS(app, origins=["http://localhost:3000", "https://workout-watcher.me", "https://workout-watcher.netlify.app", "https://635350320ae32027157667e9--workout-watcher.netlify.app"]) #allows react (localhost:3000/deployed sites) to access flask app
 
 class Action(db.Model): #setup of database
     __tablename__ = "Workouts and Reps"
